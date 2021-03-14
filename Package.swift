10bcc96b0030ccbc89630138e2de328b140b9785
Henry Betts <henry.betts@btconnect.com>
2021-03-14 22:59:11 +0000

    add CWGPU

--- Package.swift
+++ Package.swift
@@ -6,20 +6,17 @@ import PackageDescription
 let package = Package(
     name: "swift-wgpu",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "WGPU",
             targets: ["WGPU"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .systemLibrary(
+            name: "CWGPU"),
         .target(
             name: "WGPU",
-            dependencies: []),
+            dependencies: ["CWGPU"]),
     ]
 )
---
+++ Sources/CWGPU/shim.h
@@ -0,0 +1,1 @@
+#include <webgpu.h>
