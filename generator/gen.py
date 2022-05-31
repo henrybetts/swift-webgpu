245d5b3925fea227f0d3138d76c18b6c30d68256
Henry Betts <henry.betts@btconnect.com>
2022-05-31 19:30:40 +0100

    remove dawn.json and generated sources from repo

--- generator/gen.py
+++ generator/gen.py
@@ -1,27 +1,38 @@
+#!/usr/bin/env python3
+
 from genlib.model import Model
 import json
-from glob import glob
-import os
 from jinja2 import Template
+import argparse
+from pathlib import Path
 
 
 if __name__ == '__main__':
-    with open('dawn.json') as f:
-        model = Model(json.load(f), enabled_tags=['native', 'deprecated'])
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dawn-json', required=True, type=Path, help='Path to dawn.json')
+    parser.add_argument('--templates', type=Path, default=Path(__file__).parent / 'templates',
+                        help='Path to templates directory')
+    parser.add_argument('--output', type=Path, default=Path(__file__).parent / '../Sources/WebGpu/Generated',
+                        help='Path to output directory')
+    parser.add_argument('--enabled-tags', type=str, nargs='*', default=['dawn', 'native', 'deprecated'],
+                        help='Enables various tagged items to be included in the output')
 
-    template_dir = 'templates'
-    output_dir = '../Sources/WebGPU'
+    args = parser.parse_args()
+    dawn_json_path: Path = args.dawn_json
+    templates_dir: Path = args.templates
+    output_dir: Path = args.output
 
-    templates_paths = glob(os.path.join(template_dir, '*.template'))
+    with dawn_json_path.open() as f:
+        model = Model(json.load(f), enabled_tags=args.enabled_tags)
+
+    output_dir.mkdir(exist_ok=True)
+
+    templates_paths = templates_dir.glob('*.template')
 
     for template_path in templates_paths:
-        output_path = os.path.relpath(template_path, template_dir)[:-len('.template')]
-        output_path = os.path.join(output_dir, output_path)
+        template = Template(template_path.read_text(), lstrip_blocks=True, trim_blocks=True)
 
-        with open(template_path) as f:
-            template = Template(f.read(), lstrip_blocks=True, trim_blocks=True)
-
-        with open(output_path, 'w') as f:
-            f.write(template.render(model=model))
+        output_path = output_dir / template_path.stem
+        output_path.write_text(template.render(model=model))
 
         print(f'Rendered {template_path} -> {output_path}')
