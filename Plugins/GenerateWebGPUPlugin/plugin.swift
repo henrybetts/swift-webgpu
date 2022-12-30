7e25392af2f625a4838855b3e52496895a67023a
Henry Betts <henry.betts@btconnect.com>
2022-12-30 23:34:39 +0000

    Update plugin.swift

--- Plugins/GenerateWebGPUPlugin/plugin.swift
+++ Plugins/GenerateWebGPUPlugin/plugin.swift
@@ -3,15 +3,11 @@ import Foundation
 
 @main struct GenerateWebGPUPlugin: BuildToolPlugin {
     func createBuildCommands(context: PluginContext, target: Target) throws -> [Command] {
+        guard let dawnJsonEnv = ProcessInfo.processInfo.environment["DAWN_JSON"] else { return [] }
+        let dawnJson = Path(dawnJsonEnv)
+        
         let generateTool = try context.tool(named: "generate-webgpu")
         let outputDir = context.pluginWorkDirectory.appending("Generated")
-        
-        let dawnJson: Path
-        if let dawnJsonEnv = ProcessInfo.processInfo.environment["DAWN_JSON"] {
-            dawnJson = Path(dawnJsonEnv)
-        } else {
-            dawnJson = context.package.directory.appending("dawn.json")
-        }
         
         let outputFiles = [
             outputDir.appending("Enums.swift"),
@@ -30,6 +26,5 @@ import Foundation
                 arguments: ["--dawn-json", dawnJson, "--output-dir", outputDir],
                 inputFiles: [dawnJson],
                 outputFiles: outputFiles)]
-        
     }
 }
