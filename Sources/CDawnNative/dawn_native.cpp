de4e88ca71f18e825c386a4f8d71d06ab3d71a01
Henry Betts <henry@henrybetts.co.uk>
2023-11-15 13:02:20 +0000

    fix dawn native namespace

--- Sources/CDawnNative/dawn_native.cpp
+++ Sources/CDawnNative/dawn_native.cpp
@@ -4,28 +4,28 @@ extern "C" {
 #include <dawn/native/DawnNative.h>
 
 const DawnProcTable* dawnNativeGetProcs() {
-    return &dawn_native::GetProcs();
+    return &dawn::native::GetProcs();
 }
 
 DawnNativeInstance dawnNativeCreateInstance() {
-    auto instance = new dawn_native::Instance();
+    auto instance = new dawn::native::Instance();
     return reinterpret_cast<DawnNativeInstance>(instance);
 }
 
 WGPUInstance dawnNativeInstanceGet(DawnNativeInstance cInstance) {
-    auto instance = reinterpret_cast<dawn_native::Instance*>(cInstance);
+    auto instance = reinterpret_cast<dawn::native::Instance*>(cInstance);
     return instance->Get();
 }
 
 void dawnNativeInstanceEnumerateAdapters(DawnNativeInstance cInstance, size_t* adaptersCount, DawnNativeAdapter* cAdapters) {
-    auto instance = reinterpret_cast<dawn_native::Instance*>(cInstance);
+    auto instance = reinterpret_cast<dawn::native::Instance*>(cInstance);
     auto adapters = instance->EnumerateAdapters();
     if (cAdapters == NULL) {
         *adaptersCount = adapters.size();
     } else {
         size_t count = std::min(*adaptersCount, adapters.size());
         for (size_t i=0; i<count; i++) {
-            auto adapter = new dawn_native::Adapter(adapters[i]);
+            auto adapter = new dawn::native::Adapter(adapters[i]);
             cAdapters[i] = reinterpret_cast<DawnNativeAdapter>(adapter);
         }
         *adaptersCount = count;
@@ -33,26 +33,26 @@ void dawnNativeInstanceEnumerateAdapters(DawnNativeInstance cInstance, size_t* a
 }
 
 void dawnNativeInstanceRelease(DawnNativeInstance cInstance) {
-    auto instance = reinterpret_cast<dawn_native::Instance*>(cInstance);
+    auto instance = reinterpret_cast<dawn::native::Instance*>(cInstance);
     delete instance;
 }
 
 WGPUAdapter dawnNativeAdapterGet(DawnNativeAdapter cAdapter) {
-    auto adapter = reinterpret_cast<dawn_native::Adapter*>(cAdapter);
+    auto adapter = reinterpret_cast<dawn::native::Adapter*>(cAdapter);
     return adapter->Get();
 }
 
 void dawnNativeAdapterGetProperties(DawnNativeAdapter cAdapter, WGPUAdapterProperties* properties) {
-    auto adapter = reinterpret_cast<dawn_native::Adapter*>(cAdapter);
+    auto adapter = reinterpret_cast<dawn::native::Adapter*>(cAdapter);
     adapter->GetProperties(reinterpret_cast<wgpu::AdapterProperties*>(properties));
 }
 
 WGPUDevice dawnNativeAdapterCreateDevice(DawnNativeInstance cAdapter) {
-    auto adapter = reinterpret_cast<dawn_native::Adapter*>(cAdapter);
+    auto adapter = reinterpret_cast<dawn::native::Adapter*>(cAdapter);
     return adapter->CreateDevice();
 }
 
 void dawnNativeAdapterRelease(DawnNativeAdapter cAdapter) {
-    auto adapter = reinterpret_cast<dawn_native::Adapter*>(cAdapter);
+    auto adapter = reinterpret_cast<dawn::native::Adapter*>(cAdapter);
     delete adapter;
 }
