57371e67400aaa8a030d7cc901465b4d85e39e49
Graham Reeves <graham@grahamreeves.com>
2024-11-05 18:23:36 +0000

    Continue xcframework build even if removing old one fails

--- Sources/DawnNative/CreateXCFramework.sh
+++ Sources/DawnNative/CreateXCFramework.sh
@@ -15,15 +15,14 @@ MACOS_LIBRARY_PATH="${RELEASE_ROOT}/lib/libwebgpu_dawn.dylib"
 MACOS_HEADER_PATH="${RELEASE_ROOT}/include"
 DAWN_JSON_PATH="${RELEASE_ROOT}/dawn.json"
 
-# we cannot include arbritary files into an xcframework via normal means, but we can sneak them into the headers
-#cp ${DAWN_JSON_PATH} ${MACOS_HEADER_PATH}
-
-rm -r ${OUTPUT_PATH}
+# xcodebuild fails if any contents inside already exist, so clean it out
+# ||true will continue (not exit 1) if the path doesnt exist
+rm -r ${OUTPUT_PATH} || true
 
 xcodebuild -create-xcframework \
 	-library ${MACOS_LIBRARY_PATH} \
 	-headers ${MACOS_HEADER_PATH}	\
 	-output ${OUTPUT_PATH}
 
-# we cannot include arbritary files into an xcframework via normal means, but we can sneak them into the headers
+# we cannot include arbritary files into an xcframework via normal means, but we can sneak them into the output folder
 cp ${DAWN_JSON_PATH} ${OUTPUT_PATH}
